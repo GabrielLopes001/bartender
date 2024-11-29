2a89abdeddd9dd8f77d53beada0d1a5241210a95
Gabriel Lopes <gabriejob1@outlook.com>
2024-11-29 15:29:30 -0300

    Initial Commit

---
+++ bartender/bartenderApp.swift
@@ -0,0 +1,17 @@
+//
+//  bartenderApp.swift
+//  bartender
+//
+//  Created by Gabriel Lopes on 29/11/24.
+//
+
+import SwiftUI
+
+@main
+struct bartenderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
